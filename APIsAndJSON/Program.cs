a9eace0733e892a347bd1ff10167827f0975e031
Kenneth Baker <spyksaturn@gmail.com>
2023-03-06 15:00:24 -0500

    initial commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+namespace APIsAndJSON
+    
 {
+    
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Ron and Kanye Convo
+            // RonVSKanyeAPI.CarelessWhispers();
+
+            //WeatherMap Exercise
+            var client = new HttpClient();
+
+            var key = "94684d2050fd2691b4eed34a07a2679c";
+            var city = "Birmingham";
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}";
+            var response = client.GetStringAsync(weatherURL).Result;
+
+            var formattedResponse= JObject.Parse(response).GetValue("main");
+            Console.WriteLine(formattedResponse);
+            
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,35 @@ namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+        public static void KanyeQuote()
+        {
+            var kanye = new HttpClient();
+            var kanyeResponseURL = "https://api.kanye.rest/";
+            var kanyeResponse = kanye.GetStringAsync(kanyeResponseURL).Result;
+            var kanyeResponseObject = JObject.Parse(kanyeResponse);
+            Console.WriteLine($"Kanye Said: {kanyeResponseObject["quote"]}");
+            Console.WriteLine();
+        }
+        public static void RoniQuote()
+        {
+            var roni = new HttpClient(); //client
+            var roniResponseURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"; //endpoint
+            var roniResponse = roni.GetStringAsync(roniResponseURL).Result; //GET request
+            var roniResponseObject = JArray.Parse(roniResponse); //parse to string
+            Console.WriteLine($"Roni Said: {roniResponseObject[0]}");
+            Console.WriteLine();
+        }
+        public static void CarelessWhispers()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("--------------------------------------------");
+                RonVSKanyeAPI.KanyeQuote();
+                RonVSKanyeAPI.RoniQuote();
+                Console.WriteLine();
+
+            }
+        }
+        
     }
 }
